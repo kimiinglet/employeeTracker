629d2421abb353dc1b4b2a77ef19beb07220adba
kimiinglet <kinglet91391@gmail.com>
2020-01-28 10:09:30 -0600

    more stuff

--- seed.sql
+++ seed.sql
@@ -1,35 +1,32 @@
 DROP DATABASE IF EXISTS employee;
-
 CREATE DATABASE employee;
 
 USE employee;
 
--- creating new columns
-
 --Department
 CREATE TABLE  department(
--- NOT NULL - cannot be NULL
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NULL,
-  --Builds an index/ making it easier to find things
-  PRIMARY KEY (id)
+
 );
 
---role
+-- Role
 CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(7, 2),
-    department_id INT,
-    PRIMARY KEY (id)
+    department_id INT NOT FOREIGN KEY REFRRENCES (id),
+
 );
 
-CREATE TABLE employeE (
+-- Employee
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
-)
+    role_id INT NOT FOREIGN KEY REFRRENCES employee(id),
+    manager_id INT NOT FOREIGN KEY REFRRENCES role(id),
+
+);
 
 
---
+++ server.js
@@ -0,0 +1,17 @@
+//Dependencies
+const mysql = require("mysql");
+
+const connection = mysql.createCoonection){
+
+    host: "localhost",
+    
+    //your port
+    port: 3306,
+
+    //your username
+    user: "root",
+
+    //your password
+    password: "",
+    database: ""
+}
