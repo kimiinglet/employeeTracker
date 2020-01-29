076e1ad73060c1bbf848a4e37b76513d61cbad64
kimiinglet <kinglet91391@gmail.com>
2020-01-29 13:52:13 -0600

    action prompt working

--- index.js
+++ index.js
@@ -1,41 +1,130 @@
-//============== Main Question =============
+const inquirer = require("inquirer");
+
+//============== Main Question ===============
 // What would you like to do?
-    // add
-    // view
-    // update
+function action() {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "menu",
+            message: "Which action would you like to do?",
+            choices: [
+                "Add", 
+                "View", 
+                "Update",
+                "Exit"
+            ]
+        }]
+            // add
+        ).then(function (response) {
+            if(response.menu === "Add") {
+                console.log(response.menu + " selected!");
+                addTo()
+            }
+            // view
+            else if (response.menu === "View") {
+                console.log(response.menu + " selected!");
+                viewInfo()
+            }
+            //update
+            else if (response.menu === "Update") {
+                console.log(response.menu + " selected!");
+                updateInfo()
+            }
+            //exit
+            else if (response.menu === "Exit") {
+                console.log(response.menu + " selected!");
+                connection.end() 
+            }
+        })
+    }
+action();
+//action prompt working at this point
 
-//============= Add ==============
+//================== Add =====================
+// Please chose which you want to add too
+function addTo(){}
+    // Employee
+    // Role
+    // Department
+    // Back
+
+//====Employee====
+function addemployee(){}
+    //input id
+    //input first_name
+    //input last_name
+    //input manager_id
+    //input role_id
+
+    //save into table
+
+//====Role====
+function addRole(){}
+    //input id
+    //input title
+    //input salary
+    //input department_id
+
+    //save into table
+
+//====Department===
+function addDepartment(){}
+    //input id
+    //input name
+        //ex sales, accounting
+    //save into table
+
+//=================== View ===================
+// Please chose which you want to add too
+function viewInfo(){}
+    // Employee
+    // Role
+    // Department
+
+//====== Employee =====
+function viewEmployee(){}
+    // pull data from table
+
+//======= Role =======
+function viewRole(){}
+    // pull data from table
+
+//====== Department =====
+function viewDepartment(){}
+    // pull data from table
+
+
+//=================== update =================
+function updateInfo(){}
 // Please chose which you want to add too
     // Employee
     // Role
     // Department
 
 //====Employee====
+function updateEmployee(){}
+    // give list of employees
+         // choose name
+            // choose what to update
+                // update
+                    // save to table
+
 //====Role====
-//====Department===
+function updateRole(){}
+    // get list of role
+        // choose role
+            // update
+                // save to table
 
-
-//============= View ==============
-// Please chose which you want to add too
-    // Employee
-    // Role
-    // Department
-
-//====Employee====
-//====Role====
-//====Department===
-
-
-//============= update ==============
-// Please chose which you want to add too
-    // Employee
-    // Role
-    // Department
-
-    //====Employee====
-//====Role====
 //====Department====
-
+function updateDepartment(){}
+    // get list of department
+        // choose name
+            // choose what to update
+                // update
+                    // save to table
 
 
 
--- server.js
+++ server.js
@@ -12,59 +12,33 @@ let PORT = process.env.PORT || 8080;
 
 //MySQL DB connection Information 
 const connection = mysql.createCoonection({
-
-    host: "localhost",
-    
-    //your port
-    port: 3306,
-
-    //your username
-    user: "root",
-
-    //your password
-    password: "",
-    database: ""
+  host: "localhost",
+  //your port
+  port: 3306,
+  //your username
+  user: "root",
+  //your password
+  password: "P@ssword!",
+  database: "employee_db"
 });
 
 //Initiate MySQL Connection
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+  action()
+});
 
-  //Routes
-  app.get("/", function(req, res) {
 
-  });
+//Routes
+app.get("/", function (req, res) {
+});
 
-  //command line allows user to
-  
-  //add departments, roles, employees
-  function choosePosition() {};
-  
-  //view departments
-  function viewDepartments() {};
-
-  //view roles
-  function viewRoles () {};
-
-  // view employees
-  function viewEmployees() {};
-
-  //update employee roles
-  function updateEmployeeRoles() {};
+//command line allows user to
 
 
 
 
-
-
-//=============BONUS==============
-// update employee managers
-// delete departments, roles, employees,
-
-
-  
