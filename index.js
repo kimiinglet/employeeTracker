899fb86bc2804329d5c4367daba7a6e90f67f2b7
kimiinglet <kinglet91391@gmail.com>
2020-01-29 13:21:58 -0600

    all of psuedocode finished

---
+++ index.js
@@ -0,0 +1,45 @@
+//============== Main Question =============
+// What would you like to do?
+    // add
+    // view
+    // update
+
+//============= Add ==============
+// Please chose which you want to add too
+    // Employee
+    // Role
+    // Department
+
+//====Employee====
+//====Role====
+//====Department===
+
+
+//============= View ==============
+// Please chose which you want to add too
+    // Employee
+    // Role
+    // Department
+
+//====Employee====
+//====Role====
+//====Department===
+
+
+//============= update ==============
+// Please chose which you want to add too
+    // Employee
+    // Role
+    // Department
+
+    //====Employee====
+//====Role====
+//====Department====
+
+
+
+
+
+//=============BONUS==============
+// update employee managers
+// delete departments, roles, employees,
